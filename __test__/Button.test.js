7a89590a2b2d492c05de700b7a0b33e29105ae51
Lucien Perouze <lucien.perouze@cezembre.co>
2021-06-18 15:14:51 +0200

    Fix Jest config

--- __test__/Button.test.js
+++ __test__/Button.test.js
@@ -1,8 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/svelte';
 import Button from '../src/Button.svelte';
 
 test('Check content', () => {
-  const { getByText } = render(Button);
-  expect(getByText('Hello World!')).toBeInTheDocument();
+  const label = 'Hello World!';
+  const { getByText } = render(Button, { label, type: '' });
+  expect(getByText(label)).toBeInTheDocument();
 });
