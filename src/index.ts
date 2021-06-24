f9ce243a0db07c1d3b8d48e8e12f84ca5267f387
Bruno Thomas <bruno@barreverte.fr>
2021-06-24 12:43:46 +0200

    fix: Input type declaration into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import './style.scss';
 import Button from './Button.svelte';
-import Input from './TextInput.svelte';
+import TextInput from './TextInput.svelte';
 
-export { Button, Input };
+export { Button, TextInput };
