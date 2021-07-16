c465ca898cc6eb847bed99f026770d03aa898865
Bruno Thomas <bruno@barreverte.fr>
2021-07-16 08:53:05 +0200

    adds component properties propagation for TextInput

--- test/TextInput.test.js
+++ test/TextInput.test.js
@@ -1,9 +1,10 @@
 /**
  * @jest-environment jsdom
  */
-import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/svelte';
-import TextInput from '../src/TextInput.svelte';
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/svelte'
+import TextInput from '../src/TextInput.svelte'
+import TextInputSized from './TextInputSized.svelte'
 import { screen } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
 
@@ -11,30 +12,30 @@ test('Check input label', () => {
   const label = 'Hello World!';
   const { getByText } = render(TextInput, { id:'test_label', label, type: 'text' });
   expect(getByText(label)).toBeInTheDocument();
-});
+})
 
 test('Check input error', () => {
   const error = 'Hello World!';
   const { getByTestId } = render(TextInput, { id:'test_label', type: 'text', error });
   expect(getByTestId('error')).toBeInTheDocument();
-});
+})
 
 test('Check input type', () => {
   const { component } = render(TextInput, { id:'test_label', type: 'password'});
   expect(component.type).toBe("password");
-});
+})
 
 test('Check input placeholder', () => {
   const placeholder = 'Hello World!';
   const { getByPlaceholderText} = render(TextInput, { id:'test_label', type: 'text', placeholder });
   expect(getByPlaceholderText(placeholder)).toBeInTheDocument();
-});
+})
 
 test('Check input field', () => {
   const type = 'text';
   const { getByRole} = render(TextInput, { id:'test_label', type });
   expect(getByRole('textbox')).toBeInTheDocument();
-});
+})
 
 test('Check value for input text', () =>{
   render(TextInput, {id:'id', type: 'text', placeholder: 'myPlaceholder'})
@@ -43,4 +44,9 @@ test('Check value for input text', () =>{
   userEvent.type(myInput, 'blabla')
 
   expect(myInput.value).toBe('blabla')
-});
+})
+
+test('adds a custom property', () => {
+  const { getByPlaceholderText } = render(TextInputSized)
+  expect(getByPlaceholderText('ph').size).toBe(2)
+})
---
+++ test/TextInputSized.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import TextInput from "../src/TextInput.svelte";
+</script>
+
+<TextInput id="sized" placeholder="ph" size="2"></TextInput>
