cd8916bd55dd374056092219d7536d01d69d2728
Ewa <kalistaew@gmail.com>
2021-09-10 18:16:25 +0200

    adds condition to error class

--- src/TextInput.svelte
+++ src/TextInput.svelte
@@ -11,13 +11,14 @@
   function typeAction(node: HTMLInputElement) {
     node.type = type
   }
+
 </script>
 
 <div class="iroco-ui-input">
   {#if label}
     <label class="iroco-ui-label" for={id}>{label}</label>
   {/if}
-  <input on:input bind:value id={id} type="text" placeholder={placeholder}  class:error="{error !== null}" use:typeAction />
+  <input  on:input bind:value id={id} type="text" placeholder={placeholder} class="{error !== null? "error" : ''}" use:typeAction />
   {#if error}
     <p data-testid="error" class="error">{error}</p>
   {/if}
