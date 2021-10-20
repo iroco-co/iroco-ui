19c70cf4b0cd3fa663b69ed37d6836e9bd714fd5
Ewa <kalistaew@gmail.com>
2021-10-20 10:29:39 +0200

    modifies radio-button ui

--- src/RadioButton.svelte
+++ src/RadioButton.svelte
@@ -15,6 +15,7 @@
 
 <label class="iroco-ui-radio">
     <input type="radio" bind:group={ group } value={ value } name={ name } on:change="{ onChange }" { checked } />
+    <span class="radio-button-color"></span>
     <slot/>
 </label>
 
@@ -24,6 +25,33 @@
   @use "containers";
 
   .iroco-ui-radio {
-    color: colors.$beige;
-  }
+  color: colors.$beige;
+  position: relative;
+  margin-top: 0.5em;
+  padding-left:1.5em;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.iroco-ui-radio input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+.radio-button-color {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height:1em;
+  width: 1em;
+  background-color:colors.$beige;
+  border: solid 2px colors.$green;
+  border-radius: 50%;
+}
+.iroco-ui-radio input:checked ~ .radio-button-color {
+  background-color: colors.$green;
+
+}
 </style>
