3e01136c5ce48e121ef2bf10e6ee9d5dc622a753
BertrandD <volubyl@pm.me>
2021-11-12 10:57:51 +0100

    refacto: scope text input style within the component file

--- src/TextInput.svelte
+++ src/TextInput.svelte
@@ -1,32 +1,104 @@
 <script lang="ts">
-  export enum TextType { text = "text", email = "email", password = "password" }
+	export enum TextType {
+		text = 'text',
+		email = 'email',
+		password = 'password'
+	}
 
-  export let id: string;
-  export let type: TextType;
-  export let label: string | null = null;
-  export let placeholder: string | null = null;
-  export let error: string | null = null;
-  export let value: string | null = null;
-  export let onFocus: (e) => void
-  export let onBlur: (e) => void
-  export let readonly = false;
+	export let id: string;
+	export let type: TextType;
+	export let label: string | null = null;
+	export let placeholder: string | undefined
+	export let error: string | null = null;
+	export let value: string | null = null;
+	export let onFocus: (e) => void;
+	export let onBlur: (e) => void;
+	export let readonly = false;
 
-  function typeAction(node: HTMLInputElement) {
-    node.type = type
-  }
-
+	function typeAction(node: HTMLInputElement) {
+	  node.type = type;
+	}
 </script>
 
 <div class="iroco-ui-input">
-  {#if label}
-    <label class="iroco-ui-label" for={id}>{label}</label>
-  {/if}
-  <input on:input bind:value on:focus={onFocus} on:blur={onBlur} id={id} type="text" placeholder={ placeholder } class:error="{ error !== null }" class:readonlyInput="{readonly == true}" use:typeAction readonly = { readonly } />
-  {#if error !=null}
-    <p data-testid="error" class="error">{error !== null ? error: ""}</p>
-  {/if}
+	{#if label}
+		<label class="iroco-ui-label" for={id}>{label}</label>
+	{/if}
+	<input
+		on:input
+		bind:value
+		on:focus={onFocus}
+		on:blur={onBlur}
+		{id}
+		type="text"
+		{placeholder}
+		class:error={error !== null}
+		class:readonlyInput={readonly == true}
+		use:typeAction
+		{readonly}
+	/>
+	{#if error != null}
+		<p data-testid="error" class="error">{error !== null ? error : ''}</p>
+	{/if}
 </div>
 
 <style lang="scss">
-  @use "../scss/forms";
+	@use "../scss/colors";
+	input,
+	textarea {
+		outline: none;
+		text-decoration: none;
+		font-size: 14px;
+
+		&:focus {
+			outline: none;
+			text-decoration: none;
+			font-size: 14px;
+		}
+	}
+
+	.iroco-ui-input {
+		display: flex;
+		flex-direction: column;
+		> input {
+			color: colors.$beige;
+			background: colors.$darkBlue;
+			border: 1px solid colors.$darkBlue;
+			padding: 1em 1.5em;
+			text-overflow: ellipsis;
+			white-space: nowrap;
+			overflow: hidden;
+			border-radius: 0.5em;
+
+			&::placeholder {
+				color: rgba(colors.$beige, 0.5);
+			}
+			&.error {
+				border-color: colors.$red;
+			}
+			&.readonlyInput {
+				cursor: not-allowed;
+			}
+		}
+
+		.error {
+			color: colors.$red;
+		}
+	}
+
+	.iroco-ui-label {
+		color: rgba(colors.$beige, 0.6);
+		font-weight: bold;
+		padding-bottom: 10px;
+		display: inline-block;
+	}
+
+	.field {
+		margin-top: 20px;
+
+		&:first-child {
+			margin: 0;
+		}
+	}
+
 </style>
