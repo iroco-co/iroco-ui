ce42866dfb01b1d2b7313531cf061190e08dc903
Bruno Thomas <bruno@barreverte.fr>
2021-11-13 16:37:13 +0000

    adds alert to be closable

--- src/Alert.svelte
+++ src/Alert.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
 	export let type: 'success' | 'danger' = 'success';
 	export let content: string;
+	let hidden = false
+
 </script>
 
-<div class={`alert alert--${type}`}>
+<div class={`alert alert--${type}`} class:hidden={ hidden } on:click={ () => hidden = true}>
 	{content}
 </div>
 
 <style lang="scss">
 	@use '../scss/colors';
 	.alert {
-		width: 100%;
+		width: 90%;
 		min-height: 2em;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 1.5em;
-        font-weight: bold;
-        margin-bottom: 1em;
+		display: flex;
+		align-items: center;
+		border-radius: 0.5em;
+		font-size: 1.2em;
+		justify-content: center;
+		margin-bottom: 1em;
+		cursor: pointer;
 		&--danger {
 			background-color: rgba(colors.$red, 0.5);
 			border: 1px solid colors.$red;
@@ -26,5 +29,8 @@
 			background-color: rgba(colors.$green, 0.5);
 			border: 1px solid colors.$green;
 		}
+	}
+	.hidden {
+		display: none;
 	}
 </style>
