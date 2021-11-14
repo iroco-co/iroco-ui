0121d2fce3900589ea07d32adcccb02a175d95d3
Bruno Thomas <bruno@barreverte.fr>
2021-11-14 16:32:16 +0000

    fix do not hide message but give a callback to close it

--- src/Alert.svelte
+++ src/Alert.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 	export let type: 'success' | 'danger' = 'success';
 	export let content: string;
-	let hidden = false
+	export let callback: (e: Event) => void;
 
 </script>
 
-<div class={`alert alert--${type}`} class:hidden={ hidden } on:click={ () => hidden = true}>
+<div class={`alert alert--${type}`} on:click={callback} >
 	{content}
 </div>
 
 <style lang="scss">
 	@use '../scss/colors';
 	.alert {
-		width: 90%;
 		min-height: 2em;
-		display: flex;
+		display: inline-flex;
 		align-items: center;
 		border-radius: 0.5em;
+		padding: 0 1em 0 1em;
 		font-size: 1.2em;
 		justify-content: center;
 		margin-bottom: 1em;
@@ -29,8 +29,5 @@
 			background-color: rgba(colors.$green, 0.5);
 			border: 1px solid colors.$green;
 		}
-	}
-	.hidden {
-		display: none;
 	}
 </style>
--- test/Alert.test.js
+++ test/Alert.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/svelte';
+import {fireEvent, render} from '@testing-library/svelte';
 import Alert from '../src/Alert.svelte';
 
 test('Check render', () => {
@@ -17,6 +17,14 @@ test('Check render danger mode', () => {
   const elem = getByText('foo');
   expect(elem).toBeInTheDocument();
   expect(elem).toHaveClass('alert--danger')
+});
+
+test('Check close callback', () => {
+  const callback = jest.fn()
+  const { getByText } = render(Alert, { content: "foo", type : "danger", callback });
+  fireEvent.click(getByText('foo'));
+
+  expect(callback).toHaveBeenCalled();
 });
 
 
