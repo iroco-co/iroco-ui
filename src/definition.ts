77cedc9816594f27824632a399c0a408efbbc331
Bruno Thomas <bruno@barreverte.fr>
2022-11-26 18:51:34 +0100

    feat: adds color enum

--- src/definition.ts
+++ src/definition.ts
@@ -10,6 +10,20 @@ export class NavigationItem  {
     }
 }
 
+export enum Color {
+  blue= '#00B9FF',
+  darkBlue= '#211D28',
+  nightBlue= '#18151E',
+  green= '#00D692',
+  red= '#ff504d',
+  yellow= '#FFE032',
+  beige= '#f2ebe3',
+  darkBeige= '#a9a29e',
+  mediumGrey = '#464452',
+  darkGrey= '#33323a',
+  lightGrey= '#f5f5f5'
+}
+
 export enum ButtonKind {
     REGULAR = "regular",
     DANGER = "danger"
