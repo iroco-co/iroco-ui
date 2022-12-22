f2488adef2033d0972314487f361f658f2636def
Bruno Thomas <bruno@barreverte.fr>
2022-12-22 09:57:07 +0100

    ci: fix ignore package and .svelte-kit folders

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
 		'plugin:storybook/recommended'
 	],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['package/*', '*.cjs'],
+	ignorePatterns: ['*.cjs'],
 	overrides: [
 		{
 			files: ['*.svelte'],
