6ea35dc2bd2de3aae42b48e2334b823b872ab73b
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-14 18:28:05 +0100

    style: format
    Co-authored-by: Bruno Thomas <bamthomas@users.noreply.github.com>

--- src/lib/Button.stories.svelte
+++ src/lib/Button.stories.svelte
@@ -1,44 +1,42 @@
 <script context="module" type="ts">
-    import Button from './Button.svelte';
+	import Button from './Button.svelte';
 
-    export const meta = {
-        title: "Button",
-        component: Button,
-        argTypes: {
-            kind: {
-                control: {type: 'select'},
-                options: ['danger', 'success', 'dark', 'basic'],
-            },
-            size: {
-                control: {type: 'select'},
-                options: ['small', 'regular']
-            }
-        },
-    }
+	export const meta = {
+		title: 'Button',
+		component: Button,
+		argTypes: {
+			kind: {
+				control: { type: 'select' },
+				options: ['danger', 'success', 'dark', 'basic']
+			},
+			size: {
+				control: { type: 'select' },
+				options: ['small', 'regular']
+			}
+		}
+	};
 </script>
 
 <script>
-    import {Story, Template} from '@storybook/addon-svelte-csf';
+	import { Story, Template } from '@storybook/addon-svelte-csf';
 
-    let count = 0;
+	let count = 0;
 
-    function handleClick() {
-        count += 1;
-    }
+	function handleClick() {
+		count += 1;
+	}
 </script>
 
 <Template let:args>
-    <!--👇 'on:click' allows to forward event to addon-actions  -->
-    <Button {...args}
-            on:click
-            on:click={handleClick}>
-        You clicked: {count}
-    </Button>
+	<!--👇 'on:click' allows to forward event to addon-actions  -->
+	<Button {...args} on:click on:click={handleClick}>
+		You clicked: {count}
+	</Button>
 </Template>
 
-<Story name="Danger" args={{kind: "danger"}}/>
+<Story name="Danger" args={{ kind: 'danger' }} />
 
 <!-- Dynamic snippet should be disabled for this story -->
 <Story name="Button No Args">
-    <Button>Label</Button>
+	<Button>Label</Button>
 </Story>
