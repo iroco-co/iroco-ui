393e165f162e56c969fbb23ec096b5d018320ccb
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-02-23 15:40:50 +0100

    feat(TextInput): make optional/deafult props : type,placeholder,onFocus,onBlur

--- src/lib/TextInput.svelte
+++ src/lib/TextInput.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	/* eslint svelte/no-at-html-tags: "off" */
-	import type { TextInputType } from '$lib/definition';
+	import { type TextInputType } from '$lib/definition';
 
 	export let id: string;
-	export let type: TextInputType;
+	export let type: TextInputType = TextInputType.text;
 	export let name: string;
 	export let label: string | null = null;
-	export let placeholder: string | undefined;
+	export let placeholder: string | null = null;
 	export let error: string | null = null;
 	export let htmlError = false;
 	export let value: string | null = null;
-	export let onFocus: (e: FocusEvent) => void | null;
-	export let onBlur: (e: Event) => void | null;
+	export let onFocus: (e: FocusEvent) => void | null = null;
+	export let onBlur: (e: Event) => void | null = null;
 	export let readonly = false;
 	export let border = false;
 	export let autocomplete = 'on';
@@ -53,68 +53,69 @@
 </div>
 
 <style lang="scss">
-	//@use './scss/colors';
-	@use './scss/constants';
+  @use './scss/constants';
 
-	input,
-	textarea {
-		outline: none;
-		text-decoration: none;
-		font-size: 14px;
+  input,
+  textarea {
+    outline: none;
+    text-decoration: none;
+    font-size: 14px;
 
-		&:focus {
-			outline: none;
-			text-decoration: none;
-			font-size: 14px;
-		}
-	}
+    &:focus {
+      outline: none;
+      text-decoration: none;
+      font-size: 14px;
+    }
+  }
 
-	.iroco-ui-input {
-		display: flex;
-		flex-direction: column;
+  .iroco-ui-input {
+    display: flex;
+    flex-direction: column;
 
-		> input {
-			color: var(--color-text);
-			background: var(--color-body);
-			border: 1px solid var(--color-border);
-			padding: 1em 1.5em;
-			text-overflow: ellipsis;
-			white-space: nowrap;
-			overflow: hidden;
-			border-radius: constants.$border-radius;
+    > input {
+      color: var(--color-text);
+      background: var(--color-body);
+      border: 1px solid var(--color-border);
+      padding: 1em 1.5em;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+      border-radius: constants.$border-radius;
 
-			&.border {
-				border: 1px solid var(--form-element-border);
-			}
+      &.border {
+        border: 1px solid var(--form-element-border);
+      }
 
-			&::placeholder {
-				color: var(--color-text-op-50);
-			}
-			&.error {
-				border-color: var(--color-danger);
-			}
-			&.readonlyInput {
-				cursor: not-allowed;
-			}
-		}
+      &::placeholder {
+        color: var(--color-text-op-50);
+      }
 
-		.error {
-			color: var(--color-danger);
-		}
-	}
+      &.error {
+        border-color: var(--color-danger);
+      }
 
-	.iroco-ui-label {
-		color: var(--color-text-op-60);
-		font-weight: bold;
-		padding-bottom: 10px;
-		display: inline-block;
-	}
+      &.readonlyInput {
+        cursor: not-allowed;
+      }
+    }
 
-	.field {
-		margin-top: 20px;
+    .error {
+      color: var(--color-danger);
+    }
+  }
 
-		&:first-child {
-			margin: 0;
-		}
-	}
+  .iroco-ui-label {
+    color: var(--color-text-op-60);
+    font-weight: bold;
+    padding-bottom: 10px;
+    display: inline-block;
+  }
+
+  .field {
+    margin-top: 20px;
+
+    &:first-child {
+      margin: 0;
+    }
+  }
 </style>
