a3970f5613582aa9da095ed3dee6c5d9bade3b0f
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-01-10 16:39:21 +0100

    feat: full width for button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,16 +1,17 @@
 <svelte:options accessors={true} />
 
 <script lang="ts">
-	export let type = 'button';
+	export let type:'button' | 'reset' | 'submit' | null | undefined = 'button';
 	export let disabled = false;
 	export let kind: 'danger' | 'success' | 'dark' | 'basic' = 'basic';
 	export let size: 'small' | 'regular' = 'regular';
+	export let fullWidth: boolean = false;
 	export let id: string;
 </script>
 
 <button
 	{id}
-	class={`iroco-ui-button iroco-ui-button--${size} iroco-ui-button--${kind}`}
+	class={`iroco-ui-button iroco-ui-button--${size} iroco-ui-button--${kind} ${fullWidth?'iroco-ui-button--full-width':''}`}
 	class:disabled
 	{type}
 	{disabled}
