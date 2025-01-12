77ee752f41469a0e4d1a6ac13c097a6776065082
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-01-12 18:28:04 +0100

    build(svelte5): replace jsdom by happy-dom because of CommonJs issues
    This fixed the following error when running tests :
    ```
    Error: require() of ES Module /Users/marco/env/iroco/src/iroco-ui/node_modules/@asamuzakjp/css-color/dist/esm/css-color.min.js from /Users/marco/env/iroco/src/iroco-ui/node_modules/cssstyle/lib/parsers.js not supported.
    Instead change the require of css-color.min.js in /Users/marco/env/iroco/src/iroco-ui/node_modules/cssstyle/lib/parsers.js to a dynamic import() which is available in all CommonJS modules.
     ```

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,24 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig(({ mode }) => ({
-	plugins: [sveltekit()],
-	resolve: {
-		conditions: mode === 'test' ? ['browser'] : []
-	},
-	test: {
-		// jest like globals
-		globals: true,
-		environment: 'jsdom',
-		// in-source testing
-		includeSource: ['src/**/*.{js,ts,svelte}'],
-		// Add @testing-library/jest-dom matchers & mocks of SvelteKit modules
-		setupFiles: ['test/setupTest.ts'],
-		// Exclude files in c8
-		coverage: {
-			exclude: ['test/setupTest.ts']
+		plugins: [sveltekit()],
+		resolve: {
+			conditions: mode === 'test' ? ['browser'] : []
 		},
-		// Exclude playwright tests folder
-		exclude: [...configDefaults.exclude, 'tests']
-	}
-}));
+		test: {
+			// jest like globals
+			globals: true,
+			environment: 'happy-dom',
+			// in-source testing
+			includeSource: ['src/**/*.{js,ts,svelte}'],
+			// Add @testing-library/jest-dom matchers & mocks of SvelteKit modules
+			setupFiles: ['test/setupTest.ts'],
+			// Exclude files in c8
+			coverage: {
+				exclude: ['test/setupTest.ts']
+			},
+			// Exclude playwright tests folder
+			exclude: [...configDefaults.exclude, 'tests']
+		}
+	};
+});
