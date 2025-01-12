a52ce6be2e931b7618c6549bf7cfa7205312cb0e
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-01-12 18:33:47 +0100

    fix(svelte5): syntax error in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,23 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig(({ mode }) => ({
-		plugins: [sveltekit()],
-		resolve: {
-			conditions: mode === 'test' ? ['browser'] : []
+	plugins: [sveltekit()],
+	resolve: {
+		conditions: mode === 'test' ? ['browser'] : []
+	},
+	test: {
+		// jest like globals
+		globals: true,
+		environment: 'happy-dom',
+		// in-source testing
+		includeSource: ['src/**/*.{js,ts,svelte}'],
+		// Add @testing-library/jest-dom matchers & mocks of SvelteKit modules
+		setupFiles: ['test/setupTest.ts'],
+		// Exclude files in c8
+		coverage: {
+			exclude: ['test/setupTest.ts']
 		},
-		test: {
-			// jest like globals
-			globals: true,
-			environment: 'happy-dom',
-			// in-source testing
-			includeSource: ['src/**/*.{js,ts,svelte}'],
-			// Add @testing-library/jest-dom matchers & mocks of SvelteKit modules
-			setupFiles: ['test/setupTest.ts'],
-			// Exclude files in c8
-			coverage: {
-				exclude: ['test/setupTest.ts']
-			},
-			// Exclude playwright tests folder
-			exclude: [...configDefaults.exclude, 'tests']
-		}
-	};
-});
+		// Exclude playwright tests folder
+		exclude: [...configDefaults.exclude, 'tests']
+	}
+}));
