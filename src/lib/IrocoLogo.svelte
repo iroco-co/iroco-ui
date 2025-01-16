04857fad7951f3c4b9e1fa318a00e43d92f964de
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-01-16 11:02:30 +0100

    feat(svelte5): Iroco logo

--- src/lib/IrocoLogo.svelte
+++ src/lib/IrocoLogo.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	export let href = `/`;
-	export let ariaLabel = 'go to Iroco home';
-	export let width = '10rem';
-	export let height = '10rem';
+
+	let {
+		href = `/`,
+		ariaLabel = 'go to Iroco home',
+		width = '10rem',
+		height = '10rem'
+	}: {
+		href?: string;
+		ariaLabel?: string;
+		width?: string;
+		height?: string;
+	} = $props();
 </script>
 
 <a class="iroco-logo" {href} aria-label={ariaLabel}>
