744b85bc02560b243d794a3ea37ee8648b3e61d9
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-01-17 12:46:27 +0100

    docs: generate storybook static site

---
+++ docs/assets/Alert.stories-BOY43ArG.js
@@ -0,0 +1,39 @@
+import{p,i as v,f as g,a as c,b as f,t as h,s as l,c as q,d as b,e as A}from"./props-rZpV7w-1.js";import{A as o,c as _,s as N,d as P}from"./create-runtime-stories-CIwcQWO4.js";import"./index-Cu4lwwaE.js";import"./index-7SSfYZ8z.js";const C=(u,e)=>{o(u,q(()=>A(e==null?void 0:e(),[]),{children:(i,r)=>{var a=b(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+		tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+		malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed pharetra
+		nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+		Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+		sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius eu
+		augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.`);c(i,a)},$$slots:{default:!0}}))},I={title:"Iroco-UI/Atoms/Alert",component:o},{Story:t}=P();var S=h("<!> <!> <!> <!>",1);function m(u,e){p(e,!1),N(C),v();var n=S(),i=g(n);t(i,{name:"Default",parameters:{__svelteCsf:{rawCode:`<Alert {...args}>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+	tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+	malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed pharetra
+	nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+	Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+	sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius eu
+	augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
+</Alert>`}}});var r=l(i,2);t(r,{name:"Danger",args:{type:"danger"},parameters:{__svelteCsf:{rawCode:`<Alert {...args}>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+	tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+	malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed pharetra
+	nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+	Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+	sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius eu
+	augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
+</Alert>`}}});var a=l(r,2);t(a,{name:"Flash",args:{type:"flash"},parameters:{__svelteCsf:{rawCode:`<Alert {...args}>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+	tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+	malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed pharetra
+	nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+	Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+	sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius eu
+	augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
+</Alert>`}}});var d=l(a,2);t(d,{name:"Success",args:{type:"success"},parameters:{__svelteCsf:{rawCode:`<Alert {...args}>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+	tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+	malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed pharetra
+	nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+	Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+	sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius eu
+	augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
+</Alert>`}}}),c(u,n),f()}m.__docgen={version:3,name:"Alert.stories.svelte",data:[],computed:[],methods:[],components:[],description:null,keywords:[],events:[],slots:[],refs:[]};const s=_(m,I),x=["Default","Danger","Flash","Success"],y=s.Default,L=s.Danger,V=s.Flash,F=s.Success;export{L as Danger,y as Default,V as Flash,F as Success,x as __namedExportsOrder,I as default};
