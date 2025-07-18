fdab38abdda04a5a420b6013dd3412704704c4ba
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-07-18 17:56:38 +0200

    feat: simplify radio button and passthrough for standard HTML input attributes

--- src/lib/RadioButton.svelte
+++ src/lib/RadioButton.svelte
@@ -2,23 +2,17 @@
 	import type { HTMLInputAttributes } from 'svelte/elements';
 
 	interface Props extends HTMLInputAttributes {
-		value?: string | number | null;
 		group?: string | number | null;
-		name?: string | null;
-		checked?: boolean;
 		children?: import('svelte').Snippet;
 	}
 
-	let { value = null, group = $bindable(null), name = null, checked, children }: Props = $props();
+	let { group = $bindable(null), children, ...others }: Props = $props();
 
-	function onchange(event: Event) {
-		group = event.currentTarget.value;
-	}
 </script>
 
 <label class="iroco-ui-radio">
-	<input type="radio" bind:group {value} {name} {checked} {onchange} />
-	<span class="radio-button-color"></span>
+	<input type="radio" bind:group {...others} />
+	<span  class="radio-button-color"></span>
 	{@render children?.()}
 </label>
 
