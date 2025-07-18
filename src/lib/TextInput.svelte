f8bc6f2e7858bc901985ac2da63a7f1e1b8d21d8
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-07-18 17:56:21 +0200

    feat: simplify test input and passthrough for standard HTML input attributes

--- src/lib/TextInput.svelte
+++ src/lib/TextInput.svelte
@@ -1,46 +1,30 @@
 <script lang="ts">
-	import type { FormEventHandler, FullAutoFill, HTMLInputAttributes } from 'svelte/elements';
+	import type { HTMLInputAttributes } from 'svelte/elements';
 
 	export type ValidationErrorMessage = { key: string; isHtml?: boolean };
 
 	interface Props extends HTMLInputAttributes {
-		id?: string | null;
-		type?: 'email' | 'password' | 'search' | 'tel' | 'text' | 'url' | null | undefined;
-		name?: string | null;
 		label?: string | null;
-		placeholder?: string | null;
 		error?: string | null;
 		errors?: Array<ValidationErrorMessage> | null;
 		htmlError?: boolean;
-		value?: string | null;
-		onFocus?: ((e: FocusEvent) => void) | null;
-		onBlur?: ((e: Event) => void) | null;
-		readonly?: boolean;
 		border?: boolean;
-		autocomplete?: FullAutoFill | null | undefined;
-		oninput?: FormEventHandler<HTMLInputElement> | null | undefined;
 	}
 
 	let {
-		id = null,
 		type = 'text',
-		name = null,
 		label = null,
-		placeholder = null,
 		error = null,
-		errors = null,
+		errors = [],
 		htmlError = false,
-		value = $bindable(null),
-		onFocus = null,
-		onBlur = null,
-		readonly = false,
 		border = false,
 		autocomplete = 'on',
-		oninput
+		readonly,
+		...rest
 	}: Props = $props();
 
 	function hasErrors() {
-		return error !== null || errors?.length > 0;
+		return error !== null || (errors?.length ?? 0) > 0;
 	}
 </script>
 
@@ -49,19 +33,12 @@
 		<label class="iroco-ui-label" for={id}>{label}</label>
 	{/if}
 	<input
-		{oninput}
-		bind:value
-		onfocus={onFocus}
-		onblur={onBlur}
-		{id}
 		{type}
-		{name}
-		{placeholder}
 		class:border
 		class:error={hasErrors()}
 		class:readonlyInput={readonly === true}
-		{readonly}
 		{autocomplete}
+		{...rest}
 	/>
 	{#if error != null}
 		<p data-testid="error" class="error">
