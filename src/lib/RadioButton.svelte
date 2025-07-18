9424d8cb3af6908b14a9f74a778cccbd121e7a38
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-07-18 17:52:14 +0200

    feat: simplify style

--- src/lib/RadioButton.svelte
+++ src/lib/RadioButton.svelte
@@ -23,35 +23,5 @@
 </label>
 
 <style lang="scss">
-	.iroco-ui-radio {
-		color: var(--color-text);
-		position: relative;
-		margin-top: 0.5em;
-		padding-left: 1.5em;
-		cursor: pointer;
-		-webkit-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
-		user-select: none;
-	}
-
-	.iroco-ui-radio input {
-		position: absolute;
-		opacity: 0;
-		cursor: pointer;
-	}
-
-	.radio-button-color {
-		position: absolute;
-		top: 0;
-		left: 0;
-		height: 1em;
-		width: 1em;
-		background-color: var(--form-element-bg);
-		border-radius: 50%;
-	}
-
-	.iroco-ui-radio input:checked ~ .radio-button-color {
-		background-color: var(--color-primary);
-	}
+  @use './scss/forms';
 </style>
