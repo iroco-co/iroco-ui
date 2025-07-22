11a48b108976219a7ed9b10ebb2c963ba9a7ba81
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-07-22 12:25:07 +0200

    feat: fullWidth prop for Alert

--- src/lib/Alert.stories.svelte
+++ src/lib/Alert.stories.svelte
@@ -14,15 +14,15 @@
 
 {#snippet template({ ...args })}
 	<Alert {...args} onclose={() => alert('click')}>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique
-		non, ultricies eu tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique
-		magna. Aliquam id eros id ante malesuada interdum. Phasellus tristique ac leo at fringilla. Cras
-		a viverra nibh, vitae rutrum ante. Sed pharetra nibh ac velit dignissim ornare sed accumsan
-		lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a. Nullam venenatis vestibulum
-		risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales sodales
-		lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio.
-		In varius eu augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum
-		vel, tincidunt auctor lectus.
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		</p>
+		<p>
+			Nunc eros lacus, commodo eu tristique non, ultricies eu tellus.
+		</p>
+			<p>
+			Nulla facilisi. Integer a tincidunt purus.
+		</p>
 	</Alert>
 {/snippet}
 
