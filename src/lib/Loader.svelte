eab0336693a2b55b1267222079c2eee4e51db5e4
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-09-16 14:31:44 +0200

    style: format

--- src/lib/Loader.svelte
+++ src/lib/Loader.svelte
@@ -7,35 +7,33 @@
 </div>
 
 <style>
-    .width-fit-content {
-        width: fit-content;
-        margin: 0;
-				padding: 0;
-    }
+	.width-fit-content {
+		width: fit-content;
+		margin: 0;
+		padding: 0;
+	}
 
-		.transform-origin{
-        transform-origin: 2.11em 1.76em;
+	.transform-origin {
+		transform-origin: 2.11em 1.76em;
+	}
+	.rotate {
+		animation: rotation 2s;
+	}
+
+	.linear {
+		animation-timing-function: linear;
+	}
+
+	.infinite {
+		animation-iteration-count: infinite;
+	}
+
+	@keyframes rotation {
+		from {
+			transform: rotate(0deg);
 		}
-    .rotate {
-        animation: rotation 2s;
-    }
-
-    .linear {
-        animation-timing-function: linear;
-    }
-
-    .infinite {
-        animation-iteration-count: infinite;
-    }
-
-    @keyframes rotation {
-
-        from {
-
-            transform: rotate(0deg);
-        }
-        to {
-            transform: rotate(359deg);
-        }
-    }
+		to {
+			transform: rotate(359deg);
+		}
+	}
 </style>
